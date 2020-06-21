6c20aa269c73d6bfc2a7d74e593dda2b74133caa
bimbetov <beimbik-00@mail.ru>
2020-06-22 01:23:11 +0600

    Adding recyclerview with transition to a new screen with a description

--- app/src/main/java/com/example/sadaka/ui/MainActivity.kt
+++ app/src/main/java/com/example/sadaka/ui/MainActivity.kt
@@ -1,8 +1,14 @@
 package com.example.sadaka.ui
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.sadaka.R
+import com.example.sadaka.model.Needy
+import com.example.sadaka.repository.FakeNeedyRepository
+import com.example.sadaka.repository.NeedyRepository
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -10,5 +16,28 @@ class MainActivity : AppCompatActivity() {
         setTheme(R.style.AppTheme)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        val repository: NeedyRepository = FakeNeedyRepository()
+
+        recyclerView.apply {
+            adapter = NeedyAdapter(repository.getNeedy(), ::itemClick)
+            layoutManager = LinearLayoutManager(this@MainActivity)
+        }
+    }
+
+    private fun itemClick(needy: Needy){
+        val intent = Intent(this, NeedyInfoActivity::class.java).apply{
+            putExtra(IMAGE_INFO, needy.getImage().toTypedArray())
+            putExtra(NEEDED_AMOUNT, needy.getNeededAmount())
+            putExtra(COLLECTED_AMOUNT, needy.getCollectedAmount())
+        }
+        startActivity(intent)
+    }
+
+    companion object {
+        const val IMAGE_INFO: String = "IMAGE_INFO"
+        const val NEEDED_AMOUNT: String = "NEEDED_AMOUNT"
+        const val COLLECTED_AMOUNT: String = "COLLECTED_AMOUNT"
     }
 }
